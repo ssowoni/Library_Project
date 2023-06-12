14e01008ac90a29c81aca90c77fab528f633e6b7
sowon <thdnjs0946@naver.com>
2023-06-12 23:24:26 +0900

    [2023-06-12] jar -> war로 변경

--- src/main/java/com/wish/library/LibraryApplication.java
+++ src/main/java/com/wish/library/LibraryApplication.java
@@ -2,9 +2,16 @@ package com.wish.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibraryApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
